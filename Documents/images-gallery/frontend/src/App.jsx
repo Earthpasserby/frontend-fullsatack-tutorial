f29b2f6925c351afa7041cfb789b609d9c3c7f94
Earthpasserby <iwuno68@gmail.com>
2024-09-23 13:47:53 +0100

    bootstrap css

--- Documents/images-gallery/frontend/src/App.jsx
+++ Documents/images-gallery/frontend/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
