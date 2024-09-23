c8be73f8f058a4f391fd839ce8971459c20a5652
Earthpasserby <iwuno68@gmail.com>
2024-09-23 14:05:07 +0100

    header

--- Documents/images-gallery/frontend/src/components/Header.jsx
+++ Documents/images-gallery/frontend/src/components/Header.jsx
@@ -1,1 +1,11 @@
-git 
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+    </Navbar>
+  );
+};
+export default Header;
