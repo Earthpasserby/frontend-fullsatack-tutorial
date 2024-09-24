10171418f38a53b0305d76fac7e13f8f4ec6b9cf
Earthpasserby <iwuno68@gmail.com>
2024-09-24 10:46:17 +0100

    searchbar style

--- Documents/images-gallery/frontend/src/App.jsx
+++ Documents/images-gallery/frontend/src/App.jsx
@@ -4,11 +4,13 @@ import { useState } from "react";
 }
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
       <Header title="Images Gallery" />
+      <Search />
     </div>
   );
 }
--- Documents/images-gallery/frontend/src/components/Header.jsx
+++ Documents/images-gallery/frontend/src/components/Header.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+// import React from "react";
 import { Navbar } from "react-bootstrap";
+
+const navbarStyle = {
+  backgroundColor: "lightblue",
+  padding: "20px"
+};
 
 const Header = ({ title }) => {
   //destructuring
   return (
-    <Navbar bg="light" variant="light">
+    <Navbar style={navbarStyle} variant="light">
       <Navbar.Brand href="/">{title}</Navbar.Brand>
     </Navbar>
   );
